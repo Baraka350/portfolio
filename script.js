5462e3eb25592ab911718d3c05f311457868417a
baraka350 <barakahonore755@gmail.com>
2025-03-17 22:45:53 +0200

    portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ==================== typing animation =================*/
+var typed = new Typed(".typing" ,{
+    Strings:["","web Designer" , "web Devoloper" , "Graphic Designer" , "Software Devoloper"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+} )
